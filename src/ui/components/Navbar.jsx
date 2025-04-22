a8dfd43c45cbc5a429e1d1c16491e1b55311a948
Carlos <marranitocarlitos@gmail.com>
2025-04-21 20:56:38 -0400

    navbar funcionalidad de close en responsive agregada

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import { NavLink } from "react-router-dom";
 import { JojoLogo } from "../../Jojos/data/JojosData";
+import { useState } from "react";
 
 const NavbarImage = JojoLogo.find((logo) => logo.id === "JojoLogo"); 
 
 export const Navbar = () => {
+
+    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+    const clickInLink = () => {
+        if(window.innerWidth < 992){
+            setIsNavbarOpen(false);
+        }
+    }
+
+    const toggleNavbar = () => {
+        setIsNavbarOpen(!isNavbarOpen);
+    }
+
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-dark p-2">
@@ -11,20 +25,20 @@ export const Navbar = () => {
                     <img src={NavbarImage.image} alt={NavbarImage.name} className="navbar-logo" id="mi-logo" />
                 </NavLink>
 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" onClick={toggleNavbar} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className={`collapse navbar-collapse ${isNavbarOpen ? "show" : ""}`} id="navbarNav">
                     <div className="navbar-nav p-2 p-md-4 d-flex justify-content-center text-center text-lg-start w-100">
-                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="PhantomBlood">Phantom Blood</NavLink>
-                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="BattleTendency">Battle Tendency</NavLink>
-                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="StardustCrusaders">Stardust Crusaders</NavLink>
-                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="DiamondIsUnbreakable">Diamond is Unbreakable</NavLink>
-                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="GoldenWind">Golden Wind</NavLink>
-                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="StoneOcean">Stone Ocean</NavLink>
-                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="SteelBallRun">Steel Ball Run</NavLink>
-                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="JoJolion">Jojolion</NavLink>
+                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="PhantomBlood" onClick={clickInLink}>Phantom Blood</NavLink>
+                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="BattleTendency" onClick={clickInLink}>Battle Tendency</NavLink>
+                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="StardustCrusaders" onClick={clickInLink}>Stardust Crusaders</NavLink>
+                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="DiamondIsUnbreakable" onClick={clickInLink}>Diamond is Unbreakable</NavLink>
+                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="GoldenWind" onClick={clickInLink}>Golden Wind</NavLink>
+                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="StoneOcean" onClick={clickInLink}>Stone Ocean</NavLink>
+                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="SteelBallRun" onClick={clickInLink}>Steel Ball Run</NavLink>
+                        <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="JoJolion" onClick={clickInLink}>Jojolion</NavLink>
                     </div>
                 </div>
             </nav>
